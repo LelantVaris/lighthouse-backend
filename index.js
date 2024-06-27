238fd5ee48316410731d1a17171414959dbeb07f
Julian <juliankreth@gmail.com>
2024-06-27 16:26:31 +0200

    added back manual lokales load

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const puppeteer = require('puppeteer');
+const lighthouse = require('lighthouse');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-// Add a route to handle favicon requests
 app.get('/favicon.ico', (req, res) => {
-    res.status(204).end();
+  res.status(204).end();
 });
 
 app.get('/', (req, res) => {
-    res.send('Lighthouse Backend is running');
+  res.send('Lighthouse Backend is running');
 });
 
 app.post('/lighthouse', async (req, res) => {
-    const { url } = req.body;
+  const { url } = req.body;
 
-    if (!url) {
-        return res.status(400).json({ error: 'URL is required' });
-    }
+  if (!url) {
+    return res.status(400).json({ error: 'URL is required' });
+  }
 
-    try {
-        const lighthouse = await import('lighthouse');
-        const chromeLauncher = await import('chrome-launcher');
+  try {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      headless: true
+    });
 
-        const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless'] });
-        const options = { logLevel: 'info', output: 'json', onlyCategories: ['performance'], port: chrome.port };
-        const runnerResult = await lighthouse.default(url, options);
+    const options = {
+      logLevel: 'info',
+      output: 'json',
+      onlyCategories: ['performance'],
+      port: (new URL(browser.wsEndpoint())).port,
+    };
 
-        await chrome.kill();
+    const runnerResult = await lighthouse(url, options);
 
-        return res.json(runnerResult.lhr);
-    } catch (error) {
-        console.error('Error details:', error);
-        return res.status(500).json({ error: 'An error occurred while running Lighthouse', details: error.message });
-    }
+    await browser.close();
+
+    return res.json(runnerResult.lhr);
+  } catch (error) {
+    console.error('Error details:', error);
+    return res.status(500).json({ error: 'An error occurred while running Lighthouse', details: error.message });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
